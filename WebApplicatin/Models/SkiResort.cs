34a6b2e9b7bc865e0ea21c3070ed3dcb1bb75016
Anna <agan@sletat.ru>
2020-01-17 00:05:43 +0300

    Add project files.

---
+++ WebApplicatin/Models/SkiResort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicatin.Models
+{
+    public class SkiResortView
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Height { get; set; }
+        public int PathLenght { get; set; }
+    }
+}
