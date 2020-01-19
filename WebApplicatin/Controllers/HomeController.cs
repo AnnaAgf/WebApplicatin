f52bc25c917bb8578255da344aee283ce7877420
Anna <agan@sletat.ru>
2020-01-19 21:13:10 +0300

    part1

--- WebApplicatin/Controllers/HomeController.cs
+++ WebApplicatin/Controllers/HomeController.cs
@@ -3,55 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WebApplicatin.Models;
 
 namespace WebApplicatin.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly List<SkiResortView> _skiResorts = new List<SkiResortView>
-        {
-            new SkiResortView
-            {
-                Id = 1,
-                Name = "BigWood",
-                Country = "Russia",
-                Height = 1100,
-                PathLenght = 30
-                               
-            },
-            new SkiResortView
-            {
-                Id = 2,
-                Name = "Gesh",
-                Country = "Russia",
-                Height = 1500,
-                PathLenght = 15
-            },
-            new SkiResortView
-            {
-                Id = 3,
-                Name = "Elbrus",
-                Country = "Russia",
-                Height = 3800,
-                PathLenght = 10
-            }
-        };
-
-        // GET: /home/index
         public IActionResult Index()
         {
-            return View(_skiResorts);
+            return View();
         }
-
-        // GET: /home/details/{id}
-        public IActionResult Details(int id)
+        public IActionResult Blog()
         {
-            var skiResort = _skiResorts.FirstOrDefault(x => x.Id == id);
-            if (skiResort == null)
-                return NotFound();
-
-            return View(skiResort);
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
         }
     }
 }
