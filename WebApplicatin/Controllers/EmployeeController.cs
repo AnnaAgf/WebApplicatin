c3e330b791cc765c801d6d1ef42f3e9c756da596
Anna <agan@sletat.ru>
2020-01-22 19:27:56 +0300

    03-add employees

---
+++ WebApplicatin/Controllers/EmployeeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicatin.Models;
+
+namespace WebApplicatin.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Petr",
+                SurName = "Petrov",
+                Skill = "A",
+                Country = "Russia",
+                Age = 30,
+                Height = 180
+
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Max",
+                SurName = "Black",
+                Skill = "B",
+                Country = "USA",
+                Age = 31,
+                Height = 185
+            },
+            new EmployeeView
+            {
+                Id = 3,
+                FirstName = "Juan",
+                SurName = "Salvador",
+                Skill = "C",
+                Country = "Spain",
+                Age = 32,
+                Height = 175
+            }
+        };
+
+        // GET: /home/index
+        public IActionResult Index()
+        {
+            return View(_employees);
+        }
+
+        // GET: /home/details/{id}
+        public IActionResult Details(int id)
+        {
+            var employee = _employees.FirstOrDefault(x => x.Id == id);
+            if (employee == null)
+                return NotFound();
+
+            return View(employee);
+        }
+    }
+}
---
+++ WebApplicatin/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicatin.Models
+{
+    public class EmployeeView
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string Skill { get; set; }
+        public string Country { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+    }
+}
